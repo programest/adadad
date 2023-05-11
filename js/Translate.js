6be6a6e9a97be7d13e5256e718a13079033c628f
Adil <Adil>
2023-05-11 11:55:43 +0600

    11.05.2023

--- js/Translate.js
+++ js/Translate.js
@@ -15,7 +15,7 @@ var changeLocaleService = (function () {
 	function loadLocale(defLang) {
         
 		var xhr = new XMLHttpRequest();
-		xhr.open("GET", '/DMS/lang.json', true);
+		xhr.open("GET", 'lang.json', true);
 		xhr.onreadystatechange = saveLocale.bind(this);
 		xhr.onerror = function () { };
 		xhr.send();
@@ -30,7 +30,7 @@ var changeLocaleService = (function () {
 				if (cookie === 'kz') {
 					get_countries('kz')
 					changeLocale('kz')
-					document.querySelector('.nav__dropdown-flag').src = '/DMS/img/kz.png'
+					document.querySelector('.nav__dropdown-flag').src = '../img/kz.png'
 					document.querySelector('.nav__dropdown-text').innerHTML = 'Қазақша'
 				} else if (cookie === 'ru') {
 					changeLocale('ru')
@@ -38,7 +38,7 @@ var changeLocaleService = (function () {
 				} else if (cookie === 'en') {
 					changeLocale('en')
 					get_countries('en')
-					document.querySelector('.nav__dropdown-flag').src = '/DMS/img/en.png'
+					document.querySelector('.nav__dropdown-flag').src = '../img/en.png'
 					document.querySelector('.nav__dropdown-text').innerHTML = 'English'
 				} else {
 					get_countries('ru')
